42a3a2f4c4a3db69ca939f214bfc6e69b724f6aa
JazzPants <leighton.l14@gmail.com>
2021-11-05 02:21:53 +1100

    Done

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
+let num1 = 31;
+let num2 = 2;
 
 // Write your code here
+
+let multiply = num1 * num2;
+//let multiply = 31 * 2;
+console.log(multiply);
+
+// let random = Math.ceil(Math.random()); WORKS but only generates '1'
+
+let random = Math.ceil(Math.random() * 10); //1-10
+console.log(random);
+
+let num3 = 9;
+let num4 = 5;
+
+let mod = num3 % num4;
+console.log(mod);
+
+let max = Math.max(5,10,15,20);
+console.log(max);
